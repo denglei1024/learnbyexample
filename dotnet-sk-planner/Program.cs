cf2386083c8a10e3b6fbb1bd40acb3ced8178474
邓磊 <dengl02@mingyuanyun.com>
2024-04-22 21:02:34 +0800

    create

---
+++ dotnet-sk-planner/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(Env.Var("AOAI_MODEL_ID"), Env.Var("AOAI_ENDPOINT"), Env.Var("AOAI_API_KEY"));
+builder.Services.AddLogging(c => c.AddDebug().SetMinimumLevel(LogLevel.Trace));
+
+var kernel = builder.Build();
+
+var plannerConfig = new planner
+
+
